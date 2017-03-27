5de21737d0cdd4eb8a607fc0f874a757376d1b69
Chris Wolfe <chriswwolfe@gmail.com>
2017-03-27 09:07:00 -0500

    update tests

--- test/awsimandias/aws_test.clj
+++ test/awsimandias/aws_test.clj
@@ -9,10 +9,11 @@
   ;; this is gross and meant to be temporary. Assuming we have network
   ;; connectivity, check that we are able to use amazonica's stuff to actually
   ;; talk to AWS.
+  ;; this is relying on certain instances with certain properties being available in AWS.
   (let [creds (first (aws/accounts-from-env!))]
     (testing "it gets all of them when they are there"
       (let [result @(aws/all-ec2-instances! creds)]
-        (is (= [] result))))
+        (is (= 3 (count result)))))
     (testing "it gets all of ssms when they are there"
       (let [result @(aws/all-ssm-instances! creds)]
-        (is (= [] result))))))
+        (is (= 1 (count result)))))))
